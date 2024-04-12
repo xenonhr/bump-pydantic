61e13b44d507dade27e4c78cba4f56b3d04ab5de
Camillo Lugaresi <camillo@openai.com>
2024-04-11 23:11:26 -0700

    cannot use model_copy with include/exclude

--- bump_pydantic/codemods/replace_model_attribute_access.py
+++ bump_pydantic/codemods/replace_model_attribute_access.py
@@ -21,8 +21,12 @@ ATTRIBUTE_MAP = {
     "parse_raw": "model_validate_json",
 }
 
-MODEL_ATTRIBUTE=func=m.Attribute(attr=m.OneOf(*(m.Name(attr) for attr in ATTRIBUTE_MAP.keys())))
+MODEL_ATTRIBUTE=m.Attribute(attr=m.OneOf(*(m.Name(attr) for attr in ATTRIBUTE_MAP.keys())))
+COPY_CALL=m.Call(func=m.Attribute(attr=m.Name("copy")))
+ARGS_NOT_IN_MODEL_COPY=m.Arg(keyword=m.Name("exclude") | m.Name("include"))
 
+PREFIX_COMMENT = "# TODO[pydantic]: "
+INCLUDE_EXCLUDE_COMMENT = "see https://docs.pydantic.dev/latest/api/base_model/#pydantic.BaseModel.copy"
 
 class ReplaceModelAttributeAccessCommand(VisitorBasedCodemodCommand):
 
@@ -32,6 +36,7 @@ class ReplaceModelAttributeAccessCommand(VisitorBasedCodemodCommand):
         super().__init__(context)
 
         self.pydantic_model_bases = self.context.scratch[ClassDefVisitor.BASE_MODEL_CONTEXT_KEY].known_members
+        self.comment_to_add = None
 
     @m.leave(MODEL_ATTRIBUTE)
     def leave_model_attr(self, original_node: cst.Attribute, updated_node: cst.Attribute) -> cst.Attribute:
@@ -43,4 +48,28 @@ class ReplaceModelAttributeAccessCommand(VisitorBasedCodemodCommand):
             return updated_node
         if fqn in self.pydantic_model_bases:
             return updated_node.with_changes(attr=cst.Name(ATTRIBUTE_MAP[old_attr]))
+        return updated_node
+
+    @m.leave(COPY_CALL)
+    def leave_copy_call(self, original_node: cst.Call, updated_node: cst.Call) -> cst.Call:
+        if any(m.matches(arg, ARGS_NOT_IN_MODEL_COPY) for arg in original_node.args):
+            # Use `copy` instead of `model_copy`.
+            self.comment_to_add = INCLUDE_EXCLUDE_COMMENT
+            return updated_node.with_changes(
+                func=updated_node.func.with_changes(
+                    attr=cst.Name("copy"),
+                )
+            )
+        return updated_node
+
+    def leave_SimpleStatementLine(self, original_node: cst.SimpleStatementLine, updated_node: cst.SimpleStatementLine) -> cst.SimpleStatementLine:
+        if self.comment_to_add:
+            comment=cst.Comment(value=f"{PREFIX_COMMENT}{self.comment_to_add}")
+            self.comment_to_add = None
+            return updated_node.with_changes(
+                leading_lines=[
+                    *updated_node.leading_lines,
+                    cst.EmptyLine(comment=comment),
+                ]
+            )
         return updated_node
